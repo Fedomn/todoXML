820c31c62c50167c171bbcdcbae96439c1663f8a
Fedomn <shadowforfiend@gmail.com>
2015-01-03 20:43:54 +0800

    add DOM

--- src/main/java/com/fedomn/parseXML/DOMtodoXML.java
+++ src/main/java/com/fedomn/parseXML/DOMtodoXML.java
@@ -1,8 +1,85 @@
 package com.fedomn.parseXML;
 
-public class DOMtodoXML {
+import org.w3c.dom.*;
+import org.xml.sax.SAXException;
 
-    public static void main(String[] args) {
-        System.out.println();
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class DOMTodoXML {
+
+    Document document = null;
+
+    public DOMTodoXML(String xmlUrl) throws IOException, SAXException, ParserConfigurationException {
+        document = getDocument(xmlUrl);
+    }
+
+    private Document getDocument(String xmlUrl) throws ParserConfigurationException, IOException, SAXException {
+        //create DocumentBuilderFactory object
+        DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+        //create newDocumentBuilder object
+        DocumentBuilder db = dbf.newDocumentBuilder();
+        //load xml by parse method(Document : org.w3c.dom)
+        return db.parse(xmlUrl);
+    }
+
+    /**
+     * @param nodeName 结点名称
+     * @return 该名称的所以结点
+     */
+    private NodeList getNodeList(String nodeName){
+        return document.getElementsByTagName(nodeName);
+    }
+
+
+    /**
+     * 情况：不知道结点属性名，返回所有属性名+属性值
+     * @param nodeName 结点的名称
+     * @param nodeMap 解析结果存放的集合
+     */
+    public void getNodeProperty(String nodeName, Map<Integer, Map<String,String>> nodeMap){
+        NodeList nodeList = getNodeList(nodeName);
+        for (int i=0; i< nodeList.getLength(); i++){
+            //得到一个结点
+            Node node = nodeList.item(i);
+            //得到该结点的所有属性值
+            NamedNodeMap attrs = node.getAttributes();
+            //生成一个属性map
+            Map<String, String> propertyMap = new HashMap<String, String>();
+            //遍历所有属性
+            for (int j=0; j<attrs.getLength(); j++){
+                //得到一个属性
+                Node attr = attrs.item(j);
+                //插入属性名->属性值
+                propertyMap.put(attr.getNodeName(), attr.getNodeValue());
+            }
+            nodeMap.put(i+1, propertyMap);
+        }
+    }
+
+    /**
+     * 情况：知道结点属性名，返回所有属性名+属性值
+     * @param nodeName 结点的名称
+     * @param tagName 属性名
+     * @param nodeMap 解析结果存放的集合
+     */
+    public void getNodeProperty(String nodeName, String tagName, Map<Integer, Map<String,String>> nodeMap){
+        NodeList nodeList = getNodeList(nodeName);
+        for (int i=0; i < nodeList.getLength(); i++){
+            //生成一个属性map
+            Map<String, String> propertyMap = new HashMap<String, String>();
+            //将某一结点强制转换为Element类型
+            Element book = (Element) nodeList.item(i);
+            //得到tagName的属性值
+            String attrValue = book.getAttribute(tagName);
+            //保存属性名+属性值
+            propertyMap.put(tagName, attrValue);
+            nodeMap.put(i+1, propertyMap);
+        }
     }
 }
---
+++ src/test/java/DOMTests.java
@@ -0,0 +1,93 @@
+import com.fedomn.parseXML.DOMTodoXML;
+import org.junit.Before;
+import org.junit.Test;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.Assert.assertEquals;
+
+public class DOMTests {
+
+    private static final String XmlUrl = "src/main/resources/books.xml";
+
+    private static final String TagName = "id";
+
+    private DOMTodoXML doMtodoXML;
+    private Map<Integer, Map<String,String>> nodeRes = new HashMap<Integer, Map<String, String>>();
+
+    @Before
+    public void setUp() throws ParserConfigurationException, SAXException, IOException {
+        doMtodoXML = new DOMTodoXML(XmlUrl);
+    }
+
+    /**
+     * 测试1：找不到结点
+     */
+    @Test
+    public void getNodePropertyTest1(){
+        String NodeName = "NoNode";
+
+        doMtodoXML.getNodeProperty(NodeName, nodeRes);
+        assertEquals(nodeRes.isEmpty(), true);
+
+        nodeRes.clear();
+
+        doMtodoXML.getNodeProperty(NodeName, TagName, nodeRes);
+        assertEquals(nodeRes.isEmpty(), true);
+    }
+
+
+    /**
+     * 测试2：找到结点，找不到属性名
+     */
+    @Test
+    public void getNodePropertyTest2(){
+        String NodeName = "bookstore";
+        String TagName = "id";
+        doMtodoXML.getNodeProperty(NodeName, TagName, nodeRes);
+        assertEquals(nodeRes.get(1).get(TagName), "");
+    }
+
+
+    /**
+     * 测试3：找到结点，找到属性名
+     */
+    @Test
+    public void getNodePropertyTest3(){
+        String NodeName = "book";
+        String TagName = "id";
+        doMtodoXML.getNodeProperty(NodeName, TagName, nodeRes);
+
+        assertEquals(nodeRes.get(1).get(TagName), "1");
+        assertEquals(nodeRes.get(2).get(TagName), "2");
+
+//        for (Integer t : nodeRes.keySet()){
+//            System.out.println("第 "+t+"个book结点：");
+//            for (String s : nodeRes.get(t).keySet()){
+//                System.out.println("属性名："+s+" 属性值："+nodeRes.get(t).get(s));
+//            }
+//        }
+    }
+
+    /**
+     * 测试4：找到结点，打印所有属性名+属性值
+     */
+    @Test
+    public void getNodePropertyTest4(){
+        String NodeName = "book";
+        doMtodoXML.getNodeProperty(NodeName, nodeRes);
+
+        for (Integer t : nodeRes.keySet()){
+            System.out.println("第 "+t+"个"+NodeName+"结点：");
+            for (String s : nodeRes.get(t).keySet()){
+                System.out.println("属性名："+s+" 属性值："+nodeRes.get(t).get(s));
+            }
+        }
+
+    }
+
+}
