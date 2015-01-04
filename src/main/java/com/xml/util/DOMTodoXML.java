6e7c46102384fcf9dffea7f8b3c231f9497998b9
Fedomn <shadowforfiend@gmail.com>
2015-01-04 22:47:33 +0800

    add DOMwrite

--- src/main/java/com/xml/util/DOMTodoXML.java
+++ src/main/java/com/xml/util/DOMTodoXML.java
@@ -6,6 +6,10 @@ import org.xml.sax.SAXException;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.*;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+import java.io.File;
 import java.io.IOException;
 
 public final class DOMTodoXML {
@@ -18,8 +22,7 @@ public final class DOMTodoXML {
             //创建DOM解析器的工厂实例
             DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
             //从DOM工厂中获得DOM解析器
-            DocumentBuilder dbBuilder = null;
-            dbBuilder = dbFactory.newDocumentBuilder();
+            DocumentBuilder dbBuilder = dbFactory.newDocumentBuilder();
             //把要解析的xml文档读入DOM解析器
             Document doc = dbBuilder.parse(xmlUrl);
             //得到文档名称为book的元素的节点列表
@@ -93,9 +96,68 @@ public final class DOMTodoXML {
 
     }
 
+    public static void WriteXml(String xmlUrl){
+        try {
+            //创建DOM解析器的工厂实例
+            DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+            //从DOM工厂中获得DOM解析器
+            DocumentBuilder dbBuilder = dbFactory.newDocumentBuilder();
+            //创建文档树模型对象
+            Document doc = dbBuilder.newDocument();
+
+            //创建bookstore元素
+            Element bookstore = doc.createElement("bookstore");
+            //创建book元素
+            Element book = doc.createElement("book");
+            //设置元素book的属性id为001
+            book.setAttribute("id","001");
+            //创建名称为Name的元素
+            Element name = doc.createElement("name");
+            //创建名称为 设计模式 的文本节点并作为子节点添加到name元素中
+            name.appendChild(doc.createTextNode("设计模式"));
+            //将name子元素添加到book中
+            book.appendChild(name);
+
+            //添加author到book中
+            Element author = doc.createElement("author");
+            author.appendChild(doc.createTextNode("Martin Flower"));
+            book.appendChild(author);
+
+            //添加year到book中
+            Element year = doc.createElement("year");
+            year.appendChild(doc.createTextNode("2000"));
+            book.appendChild(year);
+
+            //添加price到book中
+            Element price = doc.createElement("price");
+            price.appendChild(doc.createTextNode("58"));
+            book.appendChild(price);
+
+            //bookstore添加book结点
+            bookstore.appendChild(book);
+            //添加到文档树中
+            doc.appendChild(bookstore);
+
+            //写入部分
+            TransformerFactory transformerFactory = TransformerFactory.newInstance();
+            Transformer transformer = transformerFactory.newTransformer();
+            //设置换行
+            transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+            transformer.transform(new DOMSource(doc), new StreamResult(new File(xmlUrl)));
+
+        } catch (ParserConfigurationException e) {
+            e.printStackTrace();
+        } catch (TransformerConfigurationException e) {
+            e.printStackTrace();
+        } catch (TransformerException e) {
+            e.printStackTrace();
+        }
+    }
+
+
     public static void main(String[] args){
-        String XmlUrl = "src/main/resources/books.xml";
-        ReadXml(XmlUrl);
+        ReadXml("src/main/resources/books.xml");
+        WriteXml("src/main/resources/Create_books.xml");
     }
 
 
