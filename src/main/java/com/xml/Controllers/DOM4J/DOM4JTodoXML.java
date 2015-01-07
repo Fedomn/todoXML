c4ac5cbe61f79b79adc74d54e16b00e120c925a6
Fedomn <shadowforfiend@gmail.com>
2015-01-07 22:15:46 +0800

    modified final class

--- src/main/java/com/xml/Controllers/DOM4J/DOM4JTodoXML.java
+++ src/main/java/com/xml/Controllers/DOM4J/DOM4JTodoXML.java
@@ -11,7 +11,7 @@ import java.io.File;
 import java.util.Iterator;
 import java.util.List;
 
-public class DOM4JTodoXML {
+public final class DOM4JTodoXML {
 
     public static void ReadXml(String xmlUrl, List<Book> bookList) {
         try {
