e80c54100082a747c662870342731cb439d854ec
Fedomn <shadowforfiend@gmail.com>
2015-01-10 13:18:49 +0800

    cleaning code

--- src/main/java/com/xml/Entity/Book.java
+++ src/main/java/com/xml/Entity/Book.java
@@ -2,14 +2,15 @@ package com.xml.Entity;
 
 public class Book {
 
-    private String id;
-    private String name;
-    private String author;
-    private String year;
-    private String price;
-    private String language;
+    private String id = "";
+    private String name = "";
+    private String author = "";
+    private String year = "";
+    private String price = "";
+    private String language = "";
 
-    public Book(){}
+    public Book(){
+    }
 
     public Book(String id, String name, String author, String year, String price, String language) {
         this.id = id;
@@ -92,5 +93,17 @@ public class Book {
         result = 31 * result + (price != null ? price.hashCode() : 0);
         result = 31 * result + (language != null ? language.hashCode() : 0);
         return result;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                ", author='" + author + '\'' +
+                ", year='" + year + '\'' +
+                ", price='" + price + '\'' +
+                ", language='" + language + '\'' +
+                '}';
     }
 }
--- src/test/java/com/xml/entity/BookStoreTests.java
+++ src/test/java/com/xml/entity/BookStoreTests.java
@@ -22,16 +22,18 @@ public class BookStoreTests {
 
     public static final Book testXmlBookId001;
 
+    public static String localXmlFileUrl = "src/main/resources/books.xml";
+
     static {
         localXmlBookId001 = new Book(
-                "001", "分析模式", "Martin Flower", "2000", "58", null);
+                "001", "分析模式", "Martin Flower", "2000", "58", "");
         localXmlBookId002 = new Book(
                 "002", "重构", "Martin Flower", "2000", "50", "English");
         localXmlBookId003 = new Book(
-                "003", "UML", "Martin Flower", null, "30", "Chinese");
+                "003", "UML", "Martin Flower", "", "30", "Chinese");
 
         testXmlBookId001 = new Book(
-                "001", "分析模式", "Martin Flower", "2000", "58", null);
+                "001", "分析模式", "Martin Flower", "2000", "58", "");
 
         testXmlBookStore = Arrays.asList(
                 localXmlBookId001, localXmlBookId002, localXmlBookId003);
@@ -42,7 +44,7 @@ public class BookStoreTests {
 
     @Test
     public void bookEqualsTest(){
-        assertEquals(localXmlBookId001, localXmlBookId001);
+        assertEquals(localXmlBookId001, testXmlBookId001);
 
         assertThat(testXmlBookId001.getId(), is("001"));
     }
